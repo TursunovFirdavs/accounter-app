3e3425bbeb591292a41b962350b86996343be303
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-28 03:54:25 +0500

    client page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,18 +1,67 @@
 import React from 'react'
 import account from '../../assets/User.svg'
+import { IoSearch } from "react-icons/io5";
+import { IoMdPersonAdd } from "react-icons/io";
+import { Link } from 'react-router-dom';
+
+
 
 const Profile = () => {
+    const data = [1,2,3,4,5,6,7,8,9]
   return (
     <div>
-        <div className=' bg-blue h-[278px]'></div>
-        <div>
-            <div>
+        <div className='flex gap-[50px]'>
+        <div className=' bg-blue h-[278px] w-[927px]'></div>
+        <div className='absolute top-[250px] flex items-center gap-8 left-[150px]'>
+            <div className='w-[225px] h-[225px] rounded-full border bg-white flex items-center justify-center'>
                 <img src={account} alt="" />
             </div>
             <div>
-                <p></p>
+                <p className='text-5xl pb-4 font-semibold'>Jack Bond</p>
+                <p className='text-xl font-medium'>2024-yil 21-mart</p>
             </div>
         </div>
+        <div>
+            <p className='text-lg'>Umumiy qarz</p>
+            <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+                <p className='text-3xl font-medium'>$999.999.999</p>    
+                <p className='text-3xl font-semibold'>$999.999.999</p>    
+            </div>
+            <p className='text-lg'>To’langan</p>
+            <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+                <p className='text-3xl font-medium'>$999.999.999</p>    
+                <p className='text-3xl font-semibold'>$999.999.999</p>    
+            </div>
+            <p className='text-lg'>Qolgan</p>
+            <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+                <p className='text-3xl font-medium'>$999.999.999</p>    
+                <p className='text-3xl font-semibold'>$999.999.999</p>    
+            </div>
+        </div>
+        </div>
+
+        <div className='flex justify-between px-5 border mt-[140px] items-center border-black/40'>
+            <input className='py-3 flex-1 outline-none ' type="text" placeholder='Search...' />
+            <IoSearch />
+        </div>
+
+        <div className='mt-[35px] mb-5 flex flex-col gap-3'>
+            {
+                data?.map(item => (
+                    <Link to={'/single'} className='flex bg-blue justify-between px-4 py-3 rounded-2xl' key={item}>
+                        <h3 className='text-xl font-semibold'>James Bond</h3>
+                        <div className='flex gap-10'>
+                            <p>25-mart 2024</p>
+                            <p>250000</p>
+                            <p className='text-lg font-semibold'>$9.999.999</p>
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+        <Link to={'/create'} className='w-10 h-10 rounded-full bg-white fixed flex items-center justify-center bottom-6 right-[100px]'>
+            <IoMdPersonAdd />
+        </Link>
     </div>
   )
 }
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
+import account from '../../assets/User.svg'
+import { Link } from 'react-router-dom';
+import { TiPlus } from "react-icons/ti";
+import { TiMinus } from "react-icons/ti";
+import { FaPhone } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
 
-const Single = () => {
+
+
+const Profile = () => {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
-    <div>Single</div>
+    <div>
+      <div>
+        <div className=' bg-blue h-[278px] w-full'></div>
+
+        <div className='absolute top-[250px] flex items-center gap-8 left-[150px]'>
+          <div className='w-[225px] h-[225px] rounded-full border bg-white flex items-center justify-center'>
+            <img src={account} alt="" />
+          </div>
+          <div className='flex flex-col mb-7'>
+            <p className='text-5xl pb-4 h-[110px] flex items-end w-[720px] font-semibold'>Jack Bond kdsjfnklore.</p>
+            <div>
+              <div className='text-xl font-medium flex items-center gap-1 mb-[2px]'>
+                <FaPhone className='mt-[2px]' />
+                <p>+998 99 999 99 99</p>
+              </div>
+              <p className='text-xl font-medium'>2024-yil 21-mart</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-[120px] text-xl font-semibold mb-4'>
+        <FaLocationDot className='text-xl inline-block mb-2 mr-1' />
+        Orom bozor, 10-qator, 17-magazen Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, atque doloremque optio deleniti iusto eum. Minus autem veritatis laborum hic quas omnis delectus voluptates, cupiditate quidem nostrum mollitia, voluptatibus natus.
+        </div>
+
+        <div className='flex items-center justify-between'>
+        <div>
+          <p className='text-lg'>Umumiy qarz</p>
+          <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+            <p className='text-3xl font-medium'>$999.999.999</p>
+            <p className='text-3xl font-semibold'>$999.999.999</p>
+          </div>
+          <p className='text-lg'>To’langan</p>
+          <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+            <p className='text-3xl font-medium'>$999.999.999</p>
+            <p className='text-3xl font-semibold'>$999.999.999</p>
+          </div>
+          <p className='text-lg'>Qolgan</p>
+          <div className='flex gap-[30px] pr-3 mt-1 mb-3'>
+            <p className='text-3xl font-medium'>$999.999.999</p>
+            <p className='text-3xl font-semibold'>$999.999.999</p>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-5'> 
+          <button className='text-white flex items-center justify-center gap-1 py-4 rounded-3xl text-xl font-medium w-[180px] bg-black'><TiPlus  className='text-3xl'/> Qo'shish</button>
+          <button className='text-white flex items-center justify-center gap-1 py-4 rounded-3xl text-xl font-medium w-[180px] bg-[#009FB2]'><TiMinus className='text-3xl' /> Ayirish</button>
+        </div>
+        </div>
+      </div>
+
+      <div className='mt-[35px] mb-5 flex flex-col gap-3'>
+        {
+          data?.map(item => (
+            <Link to={'/single'} className='flex bg-blue justify-between px-4 py-3 rounded-2xl' key={item}>
+              <h3 className='text-xl font-semibold'>25-mart 2024</h3>
+              <div className='flex gap-10'>
+                <p>25-mart 2024</p>
+                <p>250000</p>
+                <p className='text-lg font-semibold'>$9.999.999</p>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
-export default Single
+export default Profile
