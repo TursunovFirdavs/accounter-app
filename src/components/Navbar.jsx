77671f4e82106a3153675e432efd4bc5b9267791
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-04-03 00:14:17 +0500

    navbar media

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/hero.svg'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-4 py-6 sm:py-4 sm:px-2.5'>
-        <Link to={'/'} className='text-xl sm:text-sm font-semibold'>Company name</Link>
+        <Link to={'/'} className='flex items-center gap-2 sm:gap-1'>
+          <img className='w-[50px] sm:w-8' src={logo} alt="" />
+          <h1 className='text-xl sm:text-[16px] font-semibold'>Company name</h1>   
+        </Link>
         <div className='flex gap-3'>
             {/* <Link to={'/login'} className='text-xl font-semibold px-6 py-2 border-black border-2 rounded-xl'>Log in</Link> */}
-            <Link to={'/profile'} className='text-xl font-semibold px-6 py-2 sm:text-[10px] sm:py-0 sm:px-3 sm:border border-black border-2 rounded-xl'>Profile</Link>
+            <Link to={'/profile'} className='text-xl font-semibold px-6 py-2 sm:text-[14px] sm:py-0 sm:px-3 sm:border border-black border-2 rounded-xl'>Profile</Link>
         </div>
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import hero from '../../assets/hero.svg'
 const Home = () => {
   return (
-    <div className='bg-[#B1E3FA] xl:h-[600px] rounded-[35px] flex justify-between items-center px-6 sm:px-5 xl:gap-10 sm:flex-col sm:h-[90vh] sm:py-[37px] sm:rounded-2xl '>
+    <div className='bg-[#B1E3FA] xl:h-[600px] rounded-[35px] flex justify-between items-center px-6 sm:px-5 xl:gap-10 sm:flex-col sm:h-[85vh] sm:py-[37px] sm:rounded-2xl '>
       <div className='w-1/2 flex flex-col gap-6 sm:gap-5 sm:w-full'>
         <h1 style={{ fontFamily: 'Paytone One, sans-serif' }} className='text-[64px] sm:text-[35px] sm:leading-[40px] font-normal'>Qarzlaringizni hisoblovchi sayt!</h1>
         <p className='text-xl text-[#011B1F] font-normal sm:text-[16px] sm:font-semibold'>Lorem ipsum dolor sit amet consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum obcaecati provident ipsa fuga neque? Eveniet tenetur sapiente laboriosam repudiandae error.</p>
