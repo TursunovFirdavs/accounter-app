45a08f2511ce8df4f03e69a82916056386514280
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-04-02 22:55:10 +0500

    media

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center px-4 py-6 sm:py-3 sm:px-2.5'>
+    <div className='flex justify-between items-center px-4 py-6 sm:py-4 sm:px-2.5'>
         <Link to={'/'} className='text-xl sm:text-sm font-semibold'>Company name</Link>
         <div className='flex gap-3'>
             {/* <Link to={'/login'} className='text-xl font-semibold px-6 py-2 border-black border-2 rounded-xl'>Log in</Link> */}
