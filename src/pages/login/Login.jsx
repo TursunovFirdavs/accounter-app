4cdc91a56d9435f6eb5283212c2736e55654aa45
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-04-04 01:17:40 +0500

    shadow

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   }
 
   return (
-    <div className='w-[743px] sm:w-[352px] border m-auto px-[57px] sm:px-4 rounded-3xl mt-[50px] sm:mt-[100px]'>
+    <div className='w-[743px] sm:w-[352px] form-shadow border m-auto px-[57px] sm:px-4 rounded-3xl mt-[50px] sm:mt-[100px]'>
       <h2 className='text-3xl sm:text-2xl font-medium text-center py-7 sm:py-5 sm:mb-3'>Tizimga kirish</h2>
       <form className='flex flex-col' onSubmit={handleSubmit(submit)}>
         <label className='text-lg sm:text-[18px] mb-1' htmlFor="email">Email</label>
