8221eae2163aa55e5c9a82612c895e07c4379ac5
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-05-30 10:13:53 +0500

    home page edit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,14 +12,17 @@ const Home = () => {
   },[])
 
   return (
-    <div className='bg-[#B1E3FA] xl:h-[600px] rounded-[35px] flex justify-between items-center px-6 sm:px-5 xl:gap-10 sm:flex-col sm:h-[85vh] sm:py-[37px] sm:rounded-2xl '>
+    <div>
+      <div className='bg-[#B1E3FA] xl:h-[600px] rounded-[35px] flex justify-between items-center px-6 sm:px-5 xl:gap-10 sm:flex-col sm:h-[95vh] sm:py-[37px] sm:rounded-2xl '>
       <div className='w-1/2 flex flex-col gap-6 sm:gap-5 sm:w-full'>
         <h1 style={{ fontFamily: 'Paytone One, sans-serif' }} className='text-[64px] sm:text-[35px] sm:leading-[40px] font-normal'>Qarzlaringizni hisoblovchi sayt!</h1>
-        <p className='text-xl text-[#011B1F] font-normal sm:text-[16px] sm:font-semibold'>Lorem ipsum dolor sit amet consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum obcaecati provident ipsa fuga neque? Eveniet tenetur sapiente laboriosam repudiandae error.</p>
+        <p className='text-xl text-[#011B1F] font-normal sm:text-[16px] sm:font-semibold'>Ushbu sayt yordamida siz firmangizni umumiy abarotini, kirim, chiqim, chiqimdan qanchasi to'langanlegini, ko'chada hali qancha mablag'ingiz borlegini va omborda qancha mablag'lik tavarengiz qolganlegini juda sodda yo'l bilan hisoblab borishengiz mumkin.</p>
       </div>
       <div className='w-1/2 sm:w-[100%]'>
         <img className='w-full pb-5' src={hero} alt="" />
       </div>
+    </div>
+    <div className='h-5 xl:h-10'></div>
     </div>
   )
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -28,7 +28,8 @@ const Login = () => {
   }
 
   return (
-    <div className='w-[743px] sm:w-[352px] form-shadow border m-auto px-[57px] sm:px-4 rounded-3xl mt-[50px] sm:mt-[100px]'>
+    <div className='pb-7'>
+      <div className='w-[743px] sm:w-[352px] form-shadow border m-auto px-[57px] sm:px-4 rounded-3xl mt-[50px] sm:mt-[100px]'>
       <h2 className='text-3xl sm:text-2xl font-medium text-center py-7 sm:py-6 sm:mb-2'>Tizimga kirish</h2>
       <form className='flex flex-col' onSubmit={handleSubmit(submit)}>
         <label className='text-lg sm:text-[18px] mb-1' htmlFor="email">Name</label>
@@ -38,6 +39,8 @@ const Login = () => {
         {!isUser && <p className='text-red-600 transition'>name yoki parol xato</p>}
         <button className='bg-[#00C1CF] text-white w-[239px] text-lg font-medium sm:w-full py-3 rounded-3xl m-auto my-[60px]' type='submit'>Tasdiqlash</button>
       </form>
+    </div>
+    <div></div>
     </div>
   )
 }
